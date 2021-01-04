19603b18935067ff60e7d368b35536876fc39741
Thushan <ktwimalaweera@gmail.com>
2021-01-04 18:55:12 +0530

    View all contacts numbers saved in the phone

--- app/src/main/java/com/example/chatapp/Home.java
+++ app/src/main/java/com/example/chatapp/Home.java
@@ -2,13 +2,38 @@ package com.example.chatapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Home.this,FindUser.class));
+                finish();
+            }
+        });
+
+        getPermissions();
+    }
+
+    private void getPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS}, 1);
+        }
     }
 }
